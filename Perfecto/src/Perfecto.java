a6462f4dd9997cb6e53c2460271be1356b9057b2
MonsBaan <ibai3553@gmail.com>
2020-11-20 12:58:02 +0100

    Ejercicio: PERFECTO EscribeFicheros

--- Perfecto/src/Perfecto.java
+++ Perfecto/src/Perfecto.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Perfecto {
@@ -6,27 +9,36 @@ public class Perfecto {
 	 * 
 	 * NUMERO PERFECTO: NUMERO QUE ES IGUAL A LA SUMA DE TODOS SUS DIVISORES INCLUYENDO EL 1, PERO NO A SI MISMO
 	 * 
-	 * PROGRAMA QUE MEDIANTE LLAMADAS A LA FUNCION ANTERIOR, MUESTRE LOS NUMEROS PERFECTOS QUE HAY ENTRE 1 Y 100.00
+	 * PROGRAMA QUE MEDIANTE LLAMADAS A LA FUNCION ANTERIOR, MUESTRE LOS NUMEROS PERFECTOS QUE HAY ENTRE 1 Y 100.000
+	 * 
+	 * ESCRIBIR LOS NUMEROS PERFECTOS EN UN FICHERO LLAMADO perfectos.txt (UN NUMERO POR LINEA)
 	 */
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner sc=new Scanner(System.in);
+		PrintWriter pw;
 		int numT=0, cont=0;
-		
+
 		System.out.println("Escoge la cantidad de numeros hasta donde quieras contar los perfectos");
 		numT=sc.nextInt();
-		
-		for (int i = 1; i < numT; i++) {
-			if (esPerfecto(i)==true) {
-				System.out.println(i);
-				cont++;
-			}else if (esPerfecto(i)==false) {
-				
+		try {
+			pw=new PrintWriter(new File("perfecto.txt")); //CREACIÓN DE ARCHIVO
+			
+			for (int i = 1; i < numT; i++) {
+				if (esPerfecto(i)==true) {
+					pw.println(i); //ABRIR ARCHIVO
+					cont++;
+				}
 			}
+			pw.close(); //CERRAR ARCHIVO
+			System.out.println("Del 1 al "+numT+" hay "+cont+" numeros perfectos");
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Que has liado para que estes leyendo esto");
 		}
-		System.out.println("Del 1 al "+numT+" hay "+cont+" numeros perfectos");
+
 	}
-	
+
 	public static boolean esPerfecto(int num) {
 		int numP=0;
 		for (int i = 1; i < num; i++) {
