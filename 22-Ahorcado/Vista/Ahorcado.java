ce00e4be0ad1d18d20d07c8cea1817cc5e22b2b0
MonsBaan <ibai3553@gmail.com>
2021-01-14 12:32:17 +0100

    d

--- 22-Ahorcado/Vista/Ahorcado.java
+++ 22-Ahorcado/Vista/Ahorcado.java
@@ -4,10 +4,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import java.awt.Font;
 
 public class Ahorcado extends JFrame {
-
+	private TecladoVirtual teclado;
 	private JPanel contentPane;
+	private JLabel lblTitulo;
+	private JButton btnSiguientePalabra;
+	private JPanel panelJuego;
+	private JPanel panel1;
+	private JPanel panel2;
 
 	/**
 	 * Launch the application.
@@ -29,12 +40,34 @@ public class Ahorcado extends JFrame {
 	 * Create the frame.
 	 */
 	public Ahorcado() {
+		setTitle("El Ahorcado");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		lblTitulo = new JLabel("El Ahorcado");
+		lblTitulo.setFont(new Font("Rockwell Condensed", Font.BOLD | Font.ITALIC, 26));
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblTitulo, BorderLayout.NORTH);
+		
+		btnSiguientePalabra = new JButton("Siguiente Palabra");
+		btnSiguientePalabra.setFont(new Font("Rockwell Condensed", Font.BOLD | Font.ITALIC, 16));
+		contentPane.add(btnSiguientePalabra, BorderLayout.SOUTH);
+		
+		panelJuego = new JPanel();
+		contentPane.add(panelJuego, BorderLayout.CENTER);
+		panelJuego.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		panel1 = new JPanel();
+		panelJuego.add(panel1);
+		
+		panel2 = new JPanel();
+		panelJuego.add(panel2);
+		
+		teclado = new TecladoVirtual();
 	}
 
 }
